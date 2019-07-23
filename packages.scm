9b221d425b000975b2e0d1632658f1558b9d2e05
Carl Dong <contact@carldong.me>
2019-07-23 15:07:16 -0400

    f

--- packages.scm
+++ packages.scm
@@ -10,17 +10,19 @@
   #:use-module (guix utils))
 
 (define (make-ssp-fixed-gcc xgcc)
-  (package (inherit xgcc)
-           (source (origin (inherit (package-source xgcc))
-                           (patches
-                            (cons
-                             (local-file "patches/gcc-i686-ssp.patch")
-                             (origin-patches (package-source xgcc))))
-                           (modules (origin-modules (package-source xgcc)))
-                           (snippet
-                            (origin-snippet (package-source xgcc)))))))
+  "Given a XGCC package, return a modified package that uses the SSP function
+from glibc instead of from libssp.so. Taken from:
+http://www.linuxfromscratch.org/hlfs/view/development/chapter05/gcc-pass1.html"
+  (package
+   (inherit xgcc)
+   (arguments
+    (substitute-keyword-arguments (package-arguments xgcc)
+      ((#:make-flags flags)
+       `(cons "gcc_cv_libc_provides_ssp=yes" ,flags))))))
 
 (define (make-gcc-rpath-link xgcc)
+  "Given a XGCC package, return a modified package that replace each instance of
+-rpath in the default system spec that's inserted by Guix with -rpath-link"
   (package
    (inherit xgcc)
    (arguments
@@ -35,14 +37,11 @@
                 (("-rpath=") "-rpath-link="))
               #t))))))))
 
-(define bitcoin-gcc-9
-  (make-gcc-rpath-link (make-ssp-fixed-gcc gcc-9)))
-
-(define (cross-toolchain target
-                         base-libc
-                         base-kernel-headers
-                         base-gcc-for-libc
-                         base-gcc)
+(define (make-cross-toolchain target
+                              base-gcc-for-libc
+                              base-kernel-headers
+                              base-libc
+                              base-gcc)
   "Create a cross-compilation toolchain package for TARGET"
   (let* ((xbinutils (cross-binutils target))
          ;; 1. Build a cross-compiling gcc without targeting any libc, derived
@@ -56,7 +55,7 @@
                                         base-kernel-headers
                                         xgcc-sans-libc
                                         xbinutils))
-         ;; 3. Build cross-compiled libc with XGCC-SANS-LIBC and XKERNEL,
+         ;; 3. Build a cross-compiled libc with XGCC-SANS-LIBC and XKERNEL,
          ;; derived from BASE-LIBC
          (xlibc (cross-libc target
                             base-libc
@@ -88,40 +87,36 @@ chain for " target " development."))
       (home-page (package-home-page xgcc))
       (license (package-license xgcc)))))
 
+(define* (make-bitcoin-cross-toolchain target
+                                  #:key
+                                  (base-gcc-for-libc gcc-5)
+                                  (base-kernel-headers linux-libre-headers-4.19)
+                                  (base-libc glibc-2.27)
+                                  (base-gcc (make-gcc-rpath-link
+                                             (make-ssp-fixed-gcc gcc-9))))
+  "Convienience wrapper around MAKE-CROSS-TOOLCHAIN with default values
+desirable for building Bitcoin Core release binaries."
+  (make-cross-toolchain target
+                   base-gcc-for-libc
+                   base-kernel-headers
+                   base-libc
+                   base-gcc))
+
 (define-public guix-native-toolchain
   (make-gcc-toolchain gcc-9 glibc-2.27))
 
 (define-public xtoolchain-riscv64
-  (cross-toolchain "riscv64-linux-gnu"
-                   glibc-2.27
-                   linux-libre-headers-4.19
-                   gcc-8
-                   bitcoin-gcc-9))
+  (make-bitcoin-cross-toolchain "riscv64-linux-gnu"
+                                #:base-gcc-for-libc gcc-7))
 
 (define-public xtoolchain-x86_64
-  (cross-toolchain "x86_64-linux-gnu"
-                   glibc-2.27
-                   linux-libre-headers-4.19
-                   gcc
-                   bitcoin-gcc-9))
+  (make-bitcoin-cross-toolchain "x86_64-linux-gnu"))
 
 (define-public xtoolchain-i686
-  (cross-toolchain "i686-linux-gnu"
-                   glibc-2.27
-                   linux-libre-headers-4.19
-                   gcc
-                   bitcoin-gcc-9))
+  (make-bitcoin-cross-toolchain "i686-linux-gnu"))
 
 (define-public xtoolchain-aarch64
-  (cross-toolchain "aarch64-linux-gnu"
-                   glibc-2.27
-                   linux-libre-headers-4.19
-                   gcc
-                   bitcoin-gcc-9))
+  (make-bitcoin-cross-toolchain "aarch64-linux-gnu"))
 
 (define-public xtoolchain-armhf
-  (cross-toolchain "arm-linux-gnueabihf"
-                   glibc-2.27
-                   linux-libre-headers-4.19
-                   gcc-6
-                   bitcoin-gcc-9))
+  (make-bitcoin-cross-toolchain "arm-linux-gnueabihf"))
